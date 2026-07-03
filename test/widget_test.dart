dafd6fe0405093e13510c32c00c34469443849a4
aragumaking001-art <aragumaking001@gmail.com>
2026-07-03 14:17:14 +0900

    update

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:workmanager_flutter/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MyApp(appMode: AppMode.manager, ipAddress: "127.0.0.1"));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
